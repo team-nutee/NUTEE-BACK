93e6ddc1b1e1d776db7ef9d7acbe95c97978d386
MoonHKLee <mf311110@gmail.com>
2020-07-03 22:07:18 +0900

    NUTEE-BACK-21) add more test

--- src/test/java/kr/nutee/nuteebackend/Controller/PostControllerTest.java
+++ src/test/java/kr/nutee/nuteebackend/Controller/PostControllerTest.java
@@ -4,29 +4,41 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.nutee.nuteebackend.DTO.Request.CreatePostRequest;
 import kr.nutee.nuteebackend.DTO.Response.PostResponse;
 import kr.nutee.nuteebackend.DTO.Response.PostShowResponse;
+import kr.nutee.nuteebackend.DTO.Response.User;
+import kr.nutee.nuteebackend.Domain.Member;
+import kr.nutee.nuteebackend.Repository.MemberRepository;
 import kr.nutee.nuteebackend.Service.PostService;
+import kr.nutee.nuteebackend.Service.Util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 class PostControllerTest extends BaseControllerTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    Util util;
 
     @Test
     @DisplayName("정상적으로 포스트를 생성하는 테스트")
@@ -39,23 +51,40 @@ class PostControllerTest extends BaseControllerTest {
                 .category("IT")
                 .build();
 
-        Map<String,Object> map = new HashMap<>();
-        map.put("sub","mf3111");
-        map.put("role","ROLE_MANAGER");
-        map.put("id",2);
+        Long memberId = 2L;
+        Member member = memberRepository.findMemberById(memberId);
+        User user = User.builder()
+                .id(member.getId())
+                .nickname(member.getNickname())
+                .image(util.transferImage(member.getImage()))
+                .build();
 
         //when
+        MockHttpServletRequestBuilder builder = post("/sns/post")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", "Bearer " + token)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(body));
 
         //then
-        mockMvc.perform(post("/sns/post")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .header("Authorization","Bearer "+token)
-            .accept(MediaTypes.HAL_JSON)
-            .content(objectMapper.writeValueAsString(body))
-        )
+        mockMvc.perform(builder)
                 .andDo(print())
                 .andExpect(status().isCreated())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("title").value("제목 테스트"))
+                .andExpect(jsonPath("content").value("내용 테스트"))
+                .andExpect(jsonPath("category").value("IT"))
+                .andExpect(jsonPath("user").value(user))
+                .andExpect(jsonPath("images").isEmpty())
+                .andExpect(jsonPath("likers").isEmpty())
+                .andExpect(jsonPath("comments").isEmpty())
+                .andExpect(jsonPath("retweet").isEmpty())
+                .andExpect(jsonPath("hits").value(0))
+                .andExpect(jsonPath("blocked").value(false));
+
+
+
     }
 
 
