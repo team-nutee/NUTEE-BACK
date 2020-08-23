1dd4ccf3838e7098757c07362d231a6b18386593
MoonHKLee <mf311110@gmail.com>
2020-08-24 00:32:41 +0900

    spring cloud config

--- src/main/java/kr/nutee/nuteebackend/NuteeBackendApplication.java
+++ src/main/java/kr/nutee/nuteebackend/NuteeBackendApplication.java
@@ -1,13 +1,14 @@
 package kr.nutee.nuteebackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NuteeBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NuteeBackendApplication.class, args);
     }
-
 }
