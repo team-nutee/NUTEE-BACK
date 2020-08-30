1a93dd302acf7fc264fc066758eb57db072b77fd
MoonHKLee <mf311110@gmail.com>
2020-08-31 04:18:17 +0900

    back deploy

--- src/test/java/kr/nutee/nuteebackend/Controller/PostControllerTest.java
+++ src/test/java/kr/nutee/nuteebackend/Controller/PostControllerTest.java
@@ -55,7 +55,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Transactional
+
 public class PostControllerTest extends BaseControllerTest {
 
     @Test @Order(1)
