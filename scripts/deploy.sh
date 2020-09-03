3d5e6dfbea983015ea9396f4000cd505d1252a54
MoonHKLee <mf311110@gmail.com>
2020-09-03 21:38:36 +0900

    back deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,7 @@ echo "> 새 어플리케이션 배포"
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/build/libs/*.jar $DEPLOY/
+cp $REPOSITORY/build/libs/*.jar $DEPLOY/nutee-back.jar
 
 JAR_NAME=$(ls $DEPLOY/ |grep 'nutee-back' | tail -n 1)
 
