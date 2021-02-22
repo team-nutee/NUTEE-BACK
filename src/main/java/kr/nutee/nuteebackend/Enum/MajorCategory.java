6a1ae65a3f10898465573c6ac7200e62fcad5204
MoonHKLee <mf311110@gmail.com>
2021-02-22 17:46:24 +0900

    feature/korean -(fix) : 카테고리 한글로 변경

--- src/main/java/kr/nutee/nuteebackend/Enum/MajorCategory.java
+++ src/main/java/kr/nutee/nuteebackend/Enum/MajorCategory.java
@@ -4,25 +4,25 @@ import lombok.Getter;
 
 @Getter
 public enum MajorCategory {
-    SOCIOLOGY("SOCIOLOGY"),
-    SOCIAL_SCIENCE("SOCIAL_SCIENCE"),
-    ECONOMY("ECONOMY"),
-    POLITICS("POLITICS"),
-    BUSINESS_ADMIN("BUSINESS_ADMIN"),
-    WELFARE("WELFARE"),
-    IT("IT"),
-    INFO_COM("INFO_COM"),
-    SOFTWARE("SOFTWARE"),
-    COMPUTER_SCIENCE("COMPUTER_SCIENCE"),
-    GLOCAL_IT("GLOCAL_IT"),
-    HUMANITIES("HUMANITIES"),
-    ENGLISH("ENGLISH"),
-    JAPANESE("JAPANESE"),
-    CHINESE("CHINESE"),
-    MISSION("MISSION"),
-    MEDIA_CONTENTS("MEDIA_CONTENTS"),
-    JOURNALISM("JOURNALISM"),
-    DIGITAL_CONTENTS("DIGITAL_CONTENTS");
+    SOCIOLOGY("사회융합자율학부"),
+    SOCIAL_SCIENCE("사회학"),
+    ECONOMY("경제학"),
+    POLITICS("정치학"),
+    BUSINESS_ADMIN("경영학"),
+    WELFARE("사회복지학"),
+    IT("IT융합자율학부"),
+    INFO_COM("정보통신공학"),
+    SOFTWARE("소프트웨어공학"),
+    COMPUTER_SCIENCE("컴퓨터공학"),
+    GLOCAL_IT("글로컬IT"),
+    HUMANITIES("인문융합자율학부"),
+    ENGLISH("영어학"),
+    JAPANESE("일어일본학"),
+    CHINESE("중어중국학"),
+    MISSION("기독교문화"),
+    MEDIA_CONTENTS("미디어콘텐츠융합자율학부"),
+    JOURNALISM("신문방송학"),
+    DIGITAL_CONTENTS("디지털콘텐츠");
     private final String major;
 
     MajorCategory(String major) {
